142cd3a98d825dca343689bc8e7df430d34fa390
JoseF7830 <josefigueroam7830@gmail.com>
2023-11-22 14:31:04 -0600

    Leer ultimo mensaje del chat_log, y verificacion de busqueda de peliculas.

--- server.js
+++ server.js
@@ -23,19 +23,27 @@ app.get('/', async function(req, res) {
 
 app.post('/', async function(req, res){
     console.log(req.body);
-    const body = req.body.chat_log[0].text;
+    const body = req.body.chat_log[req.body.chat_log.length-1].text;
    
     const database = await connectDatabase();
     const collection = database.collection(process.env.COLLECTION_NAME);
     const findResult = await collection.find({ title: body }).toArray();
     // res.json(findResult);
-    res.json({"response":{
-        "text": [`Encontre la pelicula titulada ${findResult[0].title}, con año de lanzamiento ${findResult[0].year}`],
-        "response_type": "TEXT",
-        "response_options": [],
-        "stopChat": true
-    }});
-    
+    if(findResult.length == 0){
+        res.json({"response":{
+            "text": ["Disculpa no he encontrado la pelicula que buscas."],
+            "response_type": "TEXT",
+            "response_options": [],
+            "stopChat": true
+        }});
+    }else{
+        res.json({"response":{
+            "text": [`Encontre la pelicula titulada ${findResult[0].title}, con año de lanzamiento ${findResult[0].year}`],
+            "response_type": "TEXT",
+            "response_options": [],
+            "stopChat": true
+        }});
+        }
 })
 
 // app.get('/hola', (req, res) => {
